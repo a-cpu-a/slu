3b7702da9e0544a1afe898210835fcee3b67b377
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-05-28 15:22:15 +0300

    function for converting slu statements to lua statements

---
+++ slu/comp/lua/Conv.hpp
@@ -0,0 +1,24 @@
+﻿/*
+** See Copyright Notice inside Include.hpp
+*/
+#pragma once
+
+#include <string>
+#include <vector>
+#include <optional>
+#include <thread>
+#include <variant>
+#include <slu/lang/BasicState.hpp>
+
+#include <slu/comp/CompCfg.hpp>
+
+namespace slu::comp::lua
+{
+	inline void conv(const CompCfg& cfg,
+		const parse::BasicMpDbData& sharedDb,
+		parse::Output<>& out,
+		const parse::StatementV<true>& stat)
+	{
+		//TODO: Implement the conversion logic here
+	}
+}
