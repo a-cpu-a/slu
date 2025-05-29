19ad56ad0d2a4b5f77982edd46b50b547713ec72
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-05-29 19:54:40 +0300

    ignore more statements

--- slu/comp/lua/Conv.hpp
+++ slu/comp/lua/Conv.hpp
@@ -18,6 +18,15 @@ namespace slu::comp::lua
 	template<typename T>
 	concept AnyIgnoredStatement = 
 		std::same_as<T,parse::StatementType::GOTOv<true>>
+		|| std::same_as<T,parse::StatementType::SEMICOLON>
+		|| std::same_as<T,parse::StatementType::USE>
+		|| std::same_as<T,parse::StatementType::FnDeclV<true>>
+		|| std::same_as<T,parse::StatementType::FunctionDeclV<true>>
+		|| std::same_as<T,parse::StatementType::ExternBlockV<true>>//ignore, as desugaring will remove it
+		|| std::same_as<T,parse::StatementType::UnsafeBlockV<true>>//ignore, as desugaring will remove it
+		|| std::same_as<T,parse::StatementType::DROPv<true>>
+		|| std::same_as<T,parse::StatementType::MOD_DEFv<true>>
+		|| std::same_as<T,parse::StatementType::MOD_DEF_INLINEv<true>>
 		|| std::same_as<T,parse::StatementType::UNSAFE_LABEL>
 		|| std::same_as<T,parse::StatementType::SAFE_LABEL>;
 
@@ -32,6 +41,8 @@ namespace slu::comp::lua
 		ezmatch(conv.stat.data)(
 
 		varcase(const auto&) {},
+
+
 			//Ignore these
 		varcase(const AnyIgnoredStatement auto&) {}
 		);
