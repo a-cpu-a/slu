e4e132e312a7683169dfd3f518a9095554314e21
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-06-02 22:50:12 +0300

    Move the rest of empty visitor to macros

--- slu/visit/Visitor.hpp
+++ slu/visit/Visitor.hpp
@@ -23,14 +23,24 @@ namespace slu::visit
 		constexpr EmptyVisitor(SettingsT) {}
 		constexpr EmptyVisitor() = default;
 
-#define _Slu_DEF_EMPTY_POST(_Name,_Ty) void post##_Name(_Ty& itm){}
-#define _Slu_DEF_EMPTY_POST_UNIT(_Name,_Ty) void post##_Name(const _Ty itm){}
+#define _Slu_DEF_EMPTY_POST_RAW(_Name,_Ty) void post##_Name(_Ty itm){}
+#define _Slu_DEF_EMPTY_POST(_Name,_Ty) _Slu_DEF_EMPTY_POST_RAW(_Name,_Ty&)
+#define _Slu_DEF_EMPTY_POST_UNIT(_Name,_Ty) _Slu_DEF_EMPTY_POST_RAW(_Name,const _Ty)
 
-#define _Slu_DEF_EMPTY_PRE(_Name,_Ty) bool pre##_Name(_Ty& itm){return false;}
-#define _Slu_DEF_EMPTY_PRE_UNIT(_Name,_Ty) bool pre##_Name(const _Ty itm){return false;}
+#define _Slu_DEF_EMPTY_PRE_RAW(_Name,_Ty) bool pre##_Name(_Ty itm){return false;}
+#define _Slu_DEF_EMPTY_PRE(_Name,_Ty) _Slu_DEF_EMPTY_PRE_RAW(_Name,_Ty&)
+#define _Slu_DEF_EMPTY_PRE_UNIT(_Name,_Ty) _Slu_DEF_EMPTY_PRE_RAW(_Name,const _Ty)
 
 #define _Slu_DEF_EMPTY_PRE_POST(_Name,_Ty) _Slu_DEF_EMPTY_PRE(_Name,_Ty); _Slu_DEF_EMPTY_POST(_Name,_Ty);
 #define _Slu_DEF_EMPTY_AUTO(_Name)  _Slu_DEF_EMPTY_PRE_POST(_Name,parse:: _Name <Cfg>)
+
+#define _Slu_DEF_EMPTY_SEP_RAW(_Name,_Ty,_ElemTy) void sep##_Name(_Ty list,_ElemTy itm){}
+#define _Slu_DEF_EMPTY_SEP(_Name,_Ty,_ElemTy) _Slu_DEF_EMPTY_SEP_RAW(_Name,_Ty,_ElemTy&)
+
+#define _Slu_DEF_EMPTY_LIST(_Name,_ElemTy) \
+	_Slu_DEF_EMPTY_PRE_RAW(_Name,std::span<_ElemTy>) \
+	_Slu_DEF_EMPTY_POST_RAW(_Name,std::span<_ElemTy>) \
+	_Slu_DEF_EMPTY_SEP(_Name,std::span<_ElemTy>,_ElemTy)
 
 		_Slu_DEF_EMPTY_PRE_POST(File, parse::ParsedFile<Cfg>);
 		_Slu_DEF_EMPTY_AUTO(Block);
@@ -69,84 +79,23 @@ namespace slu::visit
 		_Slu_DEF_EMPTY_PRE_UNIT(Nil, parse::ExprType::NIL);
 
 		//Lists:
-		bool preDestrList(parse::PatType::DestrList<Cfg>& itm)
-		{
-			return false;
-		}
-		bool preDestrListFirst(parse::PatType::DestrList<Cfg>& itm)
-		{
-			return false;
-		}
-		void sepDestrList(std::span<parse::Pat<Cfg>> list, parse::Pat<Cfg>& itm)
-		{}
-		bool preDestrListName(parse::PatType::DestrList<Cfg>&itm)
-		{
-			return false;
-		}
-		void postDestrList(parse::PatType::DestrList<Cfg>& itm)
-		{}
+		_Slu_DEF_EMPTY_PRE(DestrList, parse::PatType::DestrList<Cfg>);
+		_Slu_DEF_EMPTY_PRE(DestrListFirst, parse::PatType::DestrList<Cfg>);
+		_Slu_DEF_EMPTY_PRE(DestrListName, parse::PatType::DestrList<Cfg>);
+		_Slu_DEF_EMPTY_POST(DestrList, parse::PatType::DestrList<Cfg>);
+		_Slu_DEF_EMPTY_SEP(DestrList, std::span<parse::Pat<Cfg>>, parse::Pat<Cfg>);
 		
-		bool preDestrFields(parse::PatType::DestrFields<Cfg>& itm)
-		{
-			return false;
-		}
-		bool preDestrFieldsFirst(parse::PatType::DestrFields<Cfg>& itm)
-		{
-			return false;
-		}
-		void sepDestrFields(std::span<parse::DestrField<Cfg>> list, parse::DestrField<Cfg>& itm)
-		{}
-		bool preDestrFieldsName(parse::PatType::DestrFields<Cfg>&itm)
-		{
-			return false;
-		}
-		void postDestrFields(parse::PatType::DestrFields<Cfg>& itm)
-		{}
+		_Slu_DEF_EMPTY_PRE(DestrFields, parse::PatType::DestrFields<Cfg>);
+		_Slu_DEF_EMPTY_PRE(DestrFieldsFirst, parse::PatType::DestrFields<Cfg>);
+		_Slu_DEF_EMPTY_PRE(DestrFieldsName, parse::PatType::DestrFields<Cfg>);
+		_Slu_DEF_EMPTY_POST(DestrFields, parse::PatType::DestrFields<Cfg>);
+		_Slu_DEF_EMPTY_SEP(DestrFields, std::span<parse::DestrField<Cfg>>, parse::DestrField<Cfg>);
 		
-		bool preVarList(std::span<parse::Var<Cfg>> itm)
-		{
-			return false;
-		}
-		void sepVarList(std::span<parse::Var<Cfg>> list, parse::Var<Cfg>& itm)
-		{}
-		void postVarList(std::span<parse::Var<Cfg>> itm)
-		{}
-		
-		bool preArgChain(std::span<parse::ArgFuncCall<Cfg>> itm)
-		{
-			return false;
-		}
-		void sepArgChain(std::span<parse::ArgFuncCall<Cfg>> list, parse::ArgFuncCall<Cfg>& itm)
-		{}
-		void postArgChain(std::span<parse::ArgFuncCall<Cfg>> itm)
-		{}
-
-		bool preExpList(std::span<parse::Expression<Cfg>> itm)
-		{
-			return false;
-		}
-		void sepExpList(std::span<parse::Expression<Cfg>> list, parse::Expression<Cfg>& itm)
-		{}
-		void postExpList(std::span<parse::Expression<Cfg>> itm)
-		{}
-
-		bool preNameList(std::span<parse::MpItmId<Cfg>> itm)
-		{
-			return false;
-		}
-		void sepNameList(std::span<parse::MpItmId<Cfg>> list, parse::MpItmId<Cfg>& itm)
-		{}
-		void postNameList(std::span<parse::MpItmId<Cfg>> itm)
-		{}
-
-		bool preParams(std::span<parse::Parameter<Cfg>> itm)
-		{
-			return false;
-		}
-		void sepParams(std::span<parse::Parameter<Cfg>> list, parse::Parameter<Cfg>& itm)
-		{}
-		void postParams(std::span<parse::Parameter<Cfg>> itm)
-		{}
+		_Slu_DEF_EMPTY_LIST(VarList, parse::Var<Cfg>);
+		_Slu_DEF_EMPTY_LIST(ArgChain, parse::ArgFuncCall<Cfg>);
+		_Slu_DEF_EMPTY_LIST(ExpList, parse::Expression<Cfg>);
+		_Slu_DEF_EMPTY_LIST(NameList, parse::MpItmId<Cfg>);
+		_Slu_DEF_EMPTY_LIST(Params, parse::Parameter<Cfg>);
 	};
 
 	/*
