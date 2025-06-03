35ba655f184d12b18e14ea9244f960ed73d3fd9c
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-06-03 21:27:24 +0300

    wip desugaring code

--- slu/midlevel/BasicDesugar.hpp
+++ slu/midlevel/BasicDesugar.hpp
@@ -11,10 +11,12 @@
 #include <slu/lang/BasicState.hpp>
 #include <slu/parser/State.hpp>
 #include <slu/visit/Visit.hpp>
+#include <slu/midlevel/Operator.hpp>
 
 namespace slu::mlvl
 {
-	struct MlvlVisitor : visit::EmptyVisitor<decltype(parse::sluCommon)>
+	using DesugarCfg = decltype(parse::sluCommon);
+	struct DesugarVisitor : visit::EmptyVisitor<DesugarCfg>
 	{
 		//TODO: Implement the conversion logic here
 		//TODO: basic desugaring:
@@ -22,11 +24,33 @@ namespace slu::mlvl
 		//TODO: auto-drop?
 		//TODO: for/while/repeat loops
 
+		bool preExpr(parse::Expression<Cfg>& itm) {
+			using MultiOp = parse::ExprType::MULTI_OPERATION<Cfg>;
+			if (std::holds_alternative<MultiOp>(itm.data))
+			{
+				MultiOp& ops = std::get<MultiOp>(itm.data);
+				_ASSERT(itm.unOps.empty());
+				_ASSERT(itm.postUnOps.empty());
+				auto order = multiOpOrder<false>(ops);
+				
+				parse::Expression<Cfg> newExpr{};
+				newExpr.place = itm.place;
+				//newExpr.data = ;
+				for (auto& i : order)
+				{
+
+				}
+
+				//desugar operators into trait func calls!
+				return true;
+			}
+			return false;
+		}; 
 	};
 
 	inline void basicDesugar(parse::ParsedFileV<true>& itm)
 	{
-		MlvlVisitor vi{};
+		DesugarVisitor vi{};
 		visit::visitFile(vi, itm);
 	}
 }
