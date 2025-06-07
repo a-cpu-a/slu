cf5341c2fbd9a9243e269d4777cb30ed68c5c578
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-06-07 21:34:00 +0300

    split lua stuff into a seperate cpp file

--- slu/SluBuildTest.cpp
+++ slu/SluBuildTest.cpp
@@ -4,26 +4,8 @@
 
 //Test macro, dont use, doesnt improve performace. (actually hurts it lol)
 //#define Slu_NoConcepts
-#define INCLUDE_LUA_C_FILES
-#include <slu/Include.hpp>
-#include <slu/ErrorType.hpp>
-#include <slu/Context.hpp>
-#include <slu/MetaTableUtils.hpp>
-#include <slu/WrapFunc.hpp>
-#include <slu/Utils.hpp>
 
-#include <slu/types/Converter.hpp>
-#include <slu/types/ReadWrite.hpp>
-#include <slu/types/TypeUtils.hpp>
-#include <slu/types/UserData.hpp>
-#include <slu/types/complex/AnyRef.hpp>
-#include <slu/types/complex/OptFunc.hpp>
-#include <slu/types/complex/Vector.hpp>
-#include <slu/types/complex/Function.hpp>
-#include <slu/types/basic/StackItem.hpp>
-#include <slu/types/basic/Optional.hpp>
-#include <slu/types/basic/RegistryRef.hpp>
-
+#include <slu/ext/lua/luaconf.h>
 #include <slu/parser/Parse.hpp>
 #include <slu/parser/VecInput.hpp>
 #include <slu/paint/Paint.hpp>
@@ -34,7 +16,7 @@
 #include <slu/comp/Compile.hpp>
 
 
-void _test()
+static void _test()
 {
 
 	slu::parse::VecInput in;
