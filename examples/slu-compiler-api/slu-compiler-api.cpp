70fa13409fdf2f3d6c759138a21269a254f7d6b4
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-06-07 18:42:59 +0300

    test project for using the compiler

---
+++ examples/slu-compiler-api/slu-compiler-api.cpp
@@ -0,0 +1,12 @@
+﻿// slu-compiler-api.cpp : Defines the entry point for the application.
+//
+
+#include "slu-compiler-api.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
