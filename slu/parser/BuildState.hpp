7273b7d84b4b44a5608ecfa299d302583a5cdd81
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-07-08 18:03:08 +0300

    BuildState to reduce boiler playe lim prefix expr stuff

---
+++ slu/parser/BuildState.hpp
@@ -0,0 +1,23 @@
+﻿/*
+** See Copyright Notice inside Include.hpp
+*/
+#pragma once
+
+#include <slu/parser/State.hpp>
+
+namespace slu::parse
+{
+	template<AnyCfgable Cfg>
+	inline std::unique_ptr<::slu::parse::LimPrefixExpr<Cfg>> mkLpe(::slu::parse::LimPrefixExpr<Cfg>&& v) {
+		return std::make_unique<::slu::parse::LimPrefixExpr<Cfg>>(v);
+	}
+	template<AnyCfgable Cfg>
+	inline std::unique_ptr<::slu::parse::LimPrefixExpr<Cfg>> mkLpeVar(::slu::parse::MpItmId<Cfg> name) {
+		return ::slu::parse::mkLpe(
+			::slu::parse::LimPrefixExprType::VAR<Cfg>{
+			.v = ::slu::parse::Var<Cfg>{
+				.base = ::slu::parse::BaseVarType::NAME<Cfg>{
+					.v = name
+		} } });
+	}
+}
