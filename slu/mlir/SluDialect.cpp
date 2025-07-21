ddeb58c394ebf5697cbb111a64e6103b0004bfa3
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-07-21 17:22:24 +0300

    add janky slu dialect.

---
+++ slu/mlir/SluDialect.cpp
@@ -0,0 +1,13 @@
+﻿#include "slu/mlir/SluDialect.h"
+
+using namespace mlir;
+using namespace slu_dial;
+
+#include "slu/mlir/SluOpsDialect.cpp.inc"
+
+void slu_dial::SluDialect::initialize() {
+    addOperations<
+#define GET_OP_LIST
+#include "slu/mlir/SluOps.cpp.inc"
+    >();
+}
---
+++ slu/mlir/SluDialect.h
@@ -0,0 +1,28 @@
+﻿
+#ifndef MLIR_DIALECT_SLU_H_
+#define MLIR_DIALECT_SLU_H_
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/VectorInterfaces.h"
+
+//===----------------------------------------------------------------------===//
+// Slu Dialect
+//===----------------------------------------------------------------------===//
+
+#include "slu/mlir/SluOpsDialect.h.inc"
+
+//===----------------------------------------------------------------------===//
+// Slu Dialect Operations
+//===----------------------------------------------------------------------===//
+
+#define GET_OP_CLASSES
+#include "slu/mlir/SluOps.h.inc"
+
+#endif // MLIR_DIALECT_SLU_H_
