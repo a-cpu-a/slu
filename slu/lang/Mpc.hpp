b02469cc148e33a01378d7404e34a37efd5926e0
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-08-18 12:53:42 +0300

    fix arr size being 0 instead of 1.

--- slu/lang/Mpc.hpp
+++ slu/lang/Mpc.hpp
@@ -35,6 +35,7 @@ namespace slu::mpc
 		}
 	};
 
+	//Warning: compiler doesnt care what N is!!
 	template<size_t N>
 	consteval auto mkMp(std::array<std::string_view, N> path,size_t id) {
 		return MpcMp{ .mp=path,.id = {id}};
@@ -42,7 +43,7 @@ namespace slu::mpc
 	consteval MpcItm mkItm(lang::ModPathId mp, size_t localId, std::string_view str) {
 		return MpcItm{ .itm = {lang::LocalObjId{localId},mp},.name = str };
 	}
-	constexpr auto MP_UNKNOWN = mkMp<0>({""sv}, 0);
+	constexpr auto MP_UNKNOWN = mkMp<1>({""sv}, 0);
 	constexpr auto MP_POOLED = MP_UNKNOWN;
 	constexpr auto MP_STD = mkMp<1>({"std"sv}, 1);
 	constexpr auto MP_STD_BOOL = mkMp<2>({"std"sv,"bool"sv}, 2);
