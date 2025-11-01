27da3a6569672694caae57a562c32bfc6c657ba6
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-11-01 13:36:33 +0200

    Print the panic msg on all platforms

--- slu/Panic.hpp
+++ slu/Panic.hpp
@@ -13,18 +13,24 @@
 #define __has_builtin(X) false
 #endif
 
+#define _Slu_panic_msg(...) \
+	std::println("Panic in " __FILE__ ":" STRINGIZE(__LINE__) " : " __VA_ARGS__)
+
 #if __has_builtin(__builtin_debugtrap)
-#define Slu_panic(...) __builtin_debugtrap()
+#define Slu_panic(...) _Slu_panic_msg(__VA_ARGS__); __builtin_debugtrap()
 #elif __has_builtin(__builtin_trap)
-#define Slu_panic(...) __builtin_trap()
+#define Slu_panic(...) _Slu_panic_msg(__VA_ARGS__); __builtin_trap()
 #elif defined(_MSC_VER)
 #if defined(_M_X64) || defined(_M_I86) || defined(_M_IX86)
-#define Slu_panic(...) std::println("Panic in " __FILE__ ":" STRINGIZE(__LINE__) " : " __VA_ARGS__);__debugbreak();std::abort() // Smaller
+#define Slu_panic(...) \
+	_Slu_panic_msg(__VA_ARGS__); \
+	__debugbreak(); /* Smaller */ \
+	std::abort()
 #else
-#define Slu_panic(...) __fastfail(0)
+#define Slu_panic(...) _Slu_panic_msg(__VA_ARGS__); __fastfail(0)
 #endif
 #else
-#define Slu_panic(...) std::abort()
+#define Slu_panic(...) _Slu_panic_msg(__VA_ARGS__); std::abort()
 #endif
 
 
